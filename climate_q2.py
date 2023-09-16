c8dcf438d82e3db7d1b7d26c1f8a4e50d776e6b5
RadioactiveW <82916350+RadioactiveW@users.noreply.github.com>
2023-09-16 20:41:12 +1000

    Climate Question2

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Using Pandas, load climate.csv; and show the data.
+
+df = pd.read_csv(r".\climate.csv")
+
 years = []
 co2 = []
 temp = []
 
+years = df['Year']
+co2 = df['CO2']
+temp = df['Temperature']
+
+
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
-
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
